8564cd2bef6114482531754e6b2d5f6d817c3fe9
Kasandra Fry <k.fry@phoenixenv.com>
2023-03-29 13:52:36 -0400

    fifth exercise solved

--- exercises/e5.js
+++ exercises/e5.js
@@ -12,7 +12,13 @@
  */
 
 // Your code goes here...
-
+const getDiffTwentySeven = (num) => {
+  if (num > 27) {
+    return (num - 27) * 2;
+  } else {
+    return (27 - num);
+  }
+}
 
 
 
@@ -24,7 +30,13 @@
  */
 
 // Your code goes here
-
+const sumOfTwoIntegers = (num1, num2) => {
+  if (num1 === num2) {
+    return (num1 + num2) * 3;
+  } else {
+    return num1 + num2;
+  }
+}
 
 
 
@@ -38,7 +50,13 @@
  */
 
 // Your code goes here...
-
+const isOneOfThemOrTheirSumIsFourty = (num1, num2) => {
+  if (num1 === 40 || num2 === 40 || num1 + num2 === 40) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 
 
